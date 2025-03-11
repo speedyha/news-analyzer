aea583a07586a147342ccc58593b090d776a6dd4
speedy <thenewamirech@gmail.com>
2025-03-11 20:44:39 +0330

    MocNewsFeed completed (client app created)

--- src/main/java/ir/jiring/newsanalyzer/MockNewsFeed.java
+++ src/main/java/ir/jiring/newsanalyzer/MockNewsFeed.java
@@ -1,14 +1,47 @@
 package ir.jiring.newsanalyzer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+
+/* MockNewsFeed generates random news items and sends them to a server at a specified host and port.
+ Each news item consists of 3 to 5 random words from a predefined list and a priority level (0-9),
+ with higher priority values being less likely to be generated.
+ The news items are sent at intervals determined by the 'newsFrequency' system property.*/
 public class MockNewsFeed {
     private static final List<String> WORDS = Arrays.asList("up", "down", "rise", "fall", "good",
             "bad", "success", "failure", "high", "low");
     private static final Random RANDOM = new Random();
+    private static final int PORT = 1234;
+    private static final String HOST = "localhost";
+    private static final Logger log = LoggerFactory.getLogger(MockNewsFeed.class);
+
+
+    public static void main(String[] args) {
+        int newsFrequency = Integer.parseInt(System.getProperty("newsFrequency", "2000"));
+
+        try (Socket socket = new Socket(HOST, PORT);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+
+            while (true) {
+                String newsItem = generateNewsItem();
+                writer.println(newsItem);
+                log.info("Sent: {}", newsItem);
+                Thread.sleep(newsFrequency);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            log.error("Error occurred", e);
+        }
+    }
 
     //    this method generates random news from 3 to 5 specific words also each news has a random priority from 0 to 9
     public static String generateNewsItem() {
